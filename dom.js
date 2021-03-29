afe0447aa7920f524d26a7e868a6d0b7341e0ebb
itdesigntk <64609807+itsisaac19@users.noreply.github.com>
2021-03-28 22:47:35 -0500

    Rush

---
+++ dom.js
@@ -0,0 +1,263 @@
+// Start 
+function startPage () {
+    document.getElementsByClassName('courseName')[0].click()
+    document.querySelector('.classind').style.opacity = '1'
+
+    $(document).ready(function() {
+        $('.js-example-basic-single').select2();
+    });
+}
+
+
+
+// Declare all helper functions here:
+function helper_parseDate (date, format) {
+    return moment(date, format).format('MMM D') 
+}
+
+function goToChildLink (el) {
+    window.open(el.children[0].classList[0])
+}
+
+function helper_createAssignmentHTML (title, dueDate, pts, desc, att) {
+    var HTML = `
+    <div class="title">`+ title +`</div>
+    <div class="points">`+ pts +`</div>
+    <div class="dueDate">`+ dueDate +`</div>
+
+    <div class="desc">`+ desc +`</div>
+    <div class="attachments">`+ att +`</div>
+   `
+
+    var pushWrap = document.createElement('div'); 
+    pushWrap.classList.add('assignment')
+    pushWrap.innerHTML = HTML
+
+    if (pushWrap.children[4].children[0]) {
+        Array.prototype.forEach.call(pushWrap.children[4].children[0].children, function(el) {
+            el.setAttribute("onclick", "goToChildLink(this)")
+        })
+    } 
+
+
+    return pushWrap
+} 
+
+function helper_createTopicHeaderHTML (name) {
+    var HTML = `
+    <div class="topicName">`+ name +`</div>
+   `
+
+    var pushWrap = document.createElement('div'); 
+    pushWrap.classList.add('topicHeader')
+    pushWrap.innerHTML = HTML
+
+    return pushWrap
+}
+
+function helper_createSideBarHTML (classTitle, id) {
+
+    var pushWrap = document.createElement('span'); 
+    pushWrap.classList.add('courseName')
+    pushWrap.classList.add(id)
+    pushWrap.innerHTML = classTitle
+
+    pushWrap.onclick = function () {
+        getCourseWork(this.classList[1]);
+        Array.prototype.forEach.call(document.getElementsByClassName('courseName'), function(el) {
+            el.classList.remove('active')
+        })
+        this.classList.add('active')
+
+        var elClicked = this
+
+        Array.prototype.forEach.call(document.querySelector('.classind').children, function(option) {
+            if (option.innerHTML == elClicked.innerHTML) {
+                document.querySelector('.classind').value = option.value
+            } 
+        })
+    }
+
+    var optEl = document.createElement('option')
+    optEl.value = classTitle
+    optEl.innerHTML = classTitle
+
+    document.querySelector('.classind').appendChild(optEl)
+
+    getCourseWork(id, true);
+    return pushWrap
+} 
+
+function helper_createAttachmentHTML (att) {
+    var pushWrap = document.createElement('button');
+    pushWrap.classList.add('attachmentItem');
+    pushWrap.classList.add('attachmentItem');
+    
+    var url;
+    var title;
+    var name = Object.keys(att)[0]
+
+    if (name == 'link') {
+        url = att.link.url
+        title = att.link.title
+    } else if (name == 'driveFile') {
+        url = att.driveFile.driveFile.alternateLink
+        title = att.driveFile.driveFile.title
+    } else if (name == 'form') {
+        url = att.form.formUrl
+        title = att.form.title
+    } else if (name == 'youtubeVideo') {
+        url = att.youtubeVideo.alternateLink
+        title = att.youtubeVideo.title
+    } 
+
+    var HTML = `
+    <div class="`+ url +`" href="`+ url +`">`+ title +`</div>
+    `
+    pushWrap.innerHTML = HTML;
+
+    return pushWrap
+}
+
+
+//////////////////////
+//////////////////////
+//////////////////////
+// Google Classroom //
+//////////////////////
+//////////////////////
+//////////////////////
+
+
+var currentCourseId
+var queryCourseId
+
+
+// Display a course topic
+function displayCourseTopic (t, passive) {
+    var name = t.name
+
+    const wrap = document.querySelector('.assignmentList');
+    const dummyWrap = document.querySelector('.DUMMYassignmentList');
+
+    if (passive == true) {
+        dummyWrap.appendChild(helper_createTopicHeaderHTML(name))
+        saveCourseWork(t.courseId, dummyWrap.innerHTML)
+        return;
+    }
+
+    wrap.appendChild(helper_createTopicHeaderHTML(name))
+    saveCourseWork(t.courseId, wrap.innerHTML)
+}
+
+// Display a coursework/material
+function displayCourseWork (work, passive, isTopicHeader, topic) {
+    const wrap = document.querySelector('.assignmentList');
+    const dummyWrap = document.querySelector('.DUMMYassignmentList');
+
+    if (work) {
+        queryCourseId = work.courseId
+    } else {
+        queryCourseId = topic.courseId
+    }
+
+
+    if (currentCourseId && currentCourseId != queryCourseId && passive == true) {
+        dummyWrap.innerHTML = null;
+    }
+
+    if (isTopicHeader == true) {
+        currentCourseId = topic.courseId;
+        displayCourseTopic(topic, passive)
+        return;
+    }
+
+    var title = work.title
+
+    var desc; desc = ''
+    if(work.description) desc = work.description
+
+    var pts = work.maxPoints
+
+    var workAtts;
+    workAtts = ''
+
+    if (work.materials) {
+        var tmpWrap = document.createElement('div')
+        Array.prototype.forEach.call(work.materials, function(mat) {
+            tmpWrap.appendChild(helper_createAttachmentHTML(mat))
+            workAtts = tmpWrap.outerHTML;
+        })
+    }
+
+    var dueDate;
+    dueDate = ''
+
+    if (work.dueDate) {
+        dueDate = 'Due ' + helper_parseDate(work.dueDate.year + ' ' + work.dueDate.month + ' ' + work.dueDate.day, 'YYYY M D') 
+    } 
+    if (work.dueTime) {
+        if (work.dueTime.minutes && work.dueTime.hours) {
+            
+        } else {
+            dueDate = dueDate + ', ' 
+            if (work.dueTime.minutes) {
+                dueDate = dueDate + ' ' + work.dueTime.hours + ':' + work.dueTime.minutes
+            }
+            if (work.dueTime.hours) {
+                dueDate = dueDate + ' ' + moment(work.dueTime.hours, 'H').format('h:mm A') 
+            }
+        }
+    }  
+
+    currentCourseId = work.courseId;
+
+    if (pts === undefined) {
+        pts = '' 
+    } else if (parseInt(pts) > 1) {
+        pts = pts + 'pts'
+    } else {
+        pts = pts + 'pt'
+    }
+
+    if (passive == true) {
+        dummyWrap.appendChild(helper_createAssignmentHTML(title, dueDate, pts, desc, workAtts))
+        saveCourseWork(work.courseId, dummyWrap.innerHTML)
+        return;
+    }
+
+    wrap.appendChild(helper_createAssignmentHTML(title, dueDate, pts, desc, workAtts))
+    saveCourseWork(work.courseId, wrap.innerHTML)
+}
+
+
+// Display a locally saved course
+function displaySavedCourseWork (id) {
+    document.querySelector('.assignmentList').innerHTML = localStorage.getItem(id)
+}
+
+// Clear the locally saved courses
+function clearSessionCourseWork () {
+    if (document.getElementsByClassName('courseName')) {
+        Array.prototype.forEach.call(document.getElementsByClassName('courseName'), function(el) {
+            var idToRemove = el.classList[1]
+            localStorage.removeItem(idToRemove)
+        })
+    }
+}
+
+// Save a course
+function saveCourseWork (id, HTML) {
+    localStorage.setItem(id, HTML)
+}
+
+
+// ALL ANIMATIONS / UI 
+
+var button = document.querySelector('.classind');
+
+button.onclick = switchClass
+
+function switchClass () {
+
+}
---
+++ gmail.js
@@ -0,0 +1,239 @@
+//  PREREQUISITES
+var CLIENT_ID = '659653087009-if2rcthrj1c6rtllo7bmsr8oioi1j24r.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyDu2DqaRCF4qCG71hhLZ-Ptlnve1fGDh-8';
+var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/classroom/v1/rest"];
+
+// Authorization scopes required by the API; multiple scopes can be included, separated by spaces.
+var SCOPES = 'https://www.googleapis.com/auth/classroom.courses.readonly https://www.googleapis.com/auth/classroom.coursework.students.readonly https://www.googleapis.com/auth/classroom.courseworkmaterials.readonly  https://www.googleapis.com/auth/classroom.topics.readonly ';
+
+
+
+gapi.load("client:auth2", function() {
+    gapi.auth2.init({
+		client_id: "659653087009-if2rcthrj1c6rtllo7bmsr8oioi1j24r.apps.googleusercontent.com"
+	}).then(function () {
+
+		gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+		updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+	
+	}, function(error) {console.log(JSON.stringify(error, null, 2));});
+});
+
+
+
+function authenticate() {
+    gapi.auth2.getAuthInstance().signIn({scope: "https://www.googleapis.com/auth/classroom.coursework.me.readonly https://www.googleapis.com/auth/classroom.coursework.students.readonly https://www.googleapis.com/auth/classroom.courseworkmaterials https://www.googleapis.com/auth/classroom.courseworkmaterials.readonly https://www.googleapis.com/auth/classroom.topics.readonly"}).then(function() { 
+        console.log("Sign-in successful"); 
+    }, function(err) {console.error("Error signing in", err);});
+}
+
+function loadClient() {
+    gapi.client.setApiKey("AIzaSyDu2DqaRCF4qCG71hhLZ-Ptlnve1fGDh-8");
+    gapi.client.load("https://classroom.googleapis.com/$discovery/rest?version=v1").then(function() { 
+        console.log("GAPI client loaded for API"); 
+		getCourseList()
+    }, function(err) { console.error("Error loading GAPI client for API", err);});
+}
+
+function updateSigninStatus(isSignedIn) {
+    if (isSignedIn) {
+		var user = gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile();
+		userThings(user);
+		loadClient()
+    } else {
+		console.log('signed out')
+		authenticate()
+    }
+}
+
+
+function userThings (user) {
+    var id = user.getId()
+    var fullName = user.getName()
+    var givenName = user.getGivenName()
+    var familyName = user.getFamilyName()
+    var imgURL = user.getImageUrl()
+    var email = user.getEmail()
+
+	console.log(email, fullName)
+}
+
+//
+//
+
+function getCourseList () {
+    gapi.client.classroom.courses.list({
+		"courseStates": [
+		  "ACTIVE"
+		],
+		"studentId": "me"
+	}).then(function(response) {
+		const courses = response.result.courses
+		var wrap = document.querySelector('.sidebar')
+		wrap.innerHTML = null
+
+
+		var last = false
+		var check;
+
+		for (var i = 0; i < courses.length; i++) {
+			(function(i) {
+				if (i == (courses.length - 1)) {
+					last = true
+				}
+
+				function appendClass () {
+					clearTimeout(check)
+
+					wrap.appendChild(helper_createSideBarHTML(courses[i].name, courses[i].id))
+					if (last == true) {
+						check = setTimeout(function(){
+							startPage()
+						}, 1500)
+						return;
+					} 
+				}
+	
+				gapi.client.classroom.courses.courseWork.list({
+					"courseId": courses[i].id,
+					"orderBy": "updateTime",
+					"pageSize": 1
+				}).then(function(response) {
+					if (response.result.courseWork == null) {
+						gapi.client.classroom.courses.courseWorkMaterials.list({
+							"courseId": courses[i].id,
+							"orderBy": "updateTime",
+							"pageSize": 1
+						}).then(function(response) {
+							if (response.result.courseWorkMaterial == null) return;
+							appendClass()
+						})
+					} else {
+						appendClass()
+					}
+					clearSessionCourseWork()
+				})
+
+
+			})(i)
+		}
+
+
+
+		console.log("Course List: ", courses);
+	})
+}
+
+function getCourseWork (CID, passive) {
+	if (localStorage.getItem(CID) && passive != true) {
+		displaySavedCourseWork(CID);
+		return;
+	} 
+	// Ok, here we go. Trying to make this as easy to read as possible.
+
+	// Make sure the DOM element is empty.
+	var wrap = document.querySelector('.assignmentList');
+    wrap.innerHTML = null
+
+
+	// Write a function to get the course topics
+	function getTheTopics () {
+		gapi.client.classroom.courses.topics.list({
+			"courseId": CID
+		}).then(function(response) {
+			// If there are no topics, STOP!
+			if (response.result.topic) {} else {
+				return;
+			}
+			// call a function 
+			getTheWork(response.result.topic)
+
+		})
+	}
+	getTheTopics()
+
+	var currentTopicName;
+
+	// Write a function to get the coursework (within a specific topic ID)
+	function getTheWork (topics) {
+		// Get the work for the course
+		gapi.client.classroom.courses.courseWork.list({
+			"courseId": CID,
+			"orderBy": "updateTime",
+		}).then(function(response) {
+			var courseWorkArray;
+			var courseWorkArrayEmpty;
+			var masterArray;
+			// If there is no work: set the array to empty
+			if (response.result.courseWork == null) {
+				courseWorkArrayEmpty = true
+				courseWorkArray = [];
+			} else {
+				courseWorkArray = response.result.courseWork;
+			}
+
+			// Get the material for the course
+			gapi.client.classroom.courses.courseWorkMaterials.list({
+				"courseId": CID,
+				"orderBy": "updateTime"
+			}).then(function(response) {
+				// If there is no course material AND no course work, STOP!
+				if (response.result.courseWorkMaterial == null && courseWorkArrayEmpty == true) return;
+
+				// Combine material and work arrays
+				array_to_push = response.result.courseWorkMaterial
+				masterArray = courseWorkArray.concat(array_to_push)
+
+				// For each topic, check if the item is in the topic:
+				topics.forEach(function(t) {
+
+					// FOR EACH ITEM IN THE COMBINED ARRAY:
+					masterArray.forEach(function(c) {
+						const courseItemId = c.topicId
+						const topicId = t.topicId
+
+						// HERE!! IF THE TOPIC ID ISN'T THE SAME AS THE ITEM'S TOPIC ID:
+						if (topicId != courseItemId) {
+							return;
+						}
+
+						// If the topic is different from the previous one:
+						if (currentTopicName) {
+							if (currentTopicName != t.name) {
+								//console.log('The topic has changed to:', t)
+								currentTopicName = t.name;
+								if (passive == true) {
+									displayCourseWork(undefined, true, true, t)
+								} else {
+									displayCourseWork(undefined, false, true, t)
+								}
+							}
+						} else {
+							currentTopicName = t.name;
+							if (passive == true) {
+								displayCourseWork(undefined, true, true, t)
+							} else {
+								displayCourseWork(undefined, false, true, t)
+							}
+						}
+						
+
+						// If we are "pre-loading", or passively loading then pass that
+						// as true into our function
+						if (passive == true) {
+							displayCourseWork(c, true)
+							return;
+						}
+
+						// Otherwise, it is a user-initiated action which we will display
+						displayCourseWork(c)
+					})
+				})
+			})
+		})
+	}
+
+}
+
+
+
